afb73facb7a4e85683f1947bebce1b05d9a4d9d9
sohaibakh <soaibakh@gmail.com>
2026-07-12 01:34:57 +0500

    more pages

---
+++ src/app/admin/tailors/page.tsx
@@ -0,0 +1,5 @@
+import { AdminPage } from "@/components/common/pages";
+
+export default function Page() {
+  return <AdminPage view="tailors" />;
+}
---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+import { AuthPage } from "@/components/common/pages";
+
+export default function Page() {
+  return <AuthPage mode="forgot" />;
+}
